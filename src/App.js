d3b0681264b582ccac964d9e87e7f56e7b1e7e82
Adam <adam@mradam.us>
2021-09-01 08:59:30 -0500

    working on the form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid, Typography } from '@material-ui/core';
+import Textfield from './Components/FormsUI/Textfield';
+import Select from './Components/FormsUI/Select';
+import countries from './data/countries.json';
 
 const useStyles = makeStyles((theme) => ({
   formWrapper: {
@@ -11,9 +14,32 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(8),
   },
 }));
-const INITIAL_FORM_STATE = {};
+const INITIAL_FORM_STATE = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+  addressLine1: '',
+  addressLine2: '',
+  city: '',
+  state: '',
+  country: '',
+};
 
-const FORM_VALIDATION = Yup.object().shape({});
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required('Required'),
+  lastName: Yup.string().required('Required'),
+  email: Yup.string().email('Invalid email.').required('Required'),
+  phone: Yup.number()
+    .integer()
+    .typeError('Please enter a valid phone number')
+    .required('Required'),
+  addressLine1: Yup.string().required('Required'),
+  addressLine2: Yup.string(),
+  city: Yup.string().required('Required'),
+  state: Yup.string().required('Required'),
+  country: Yup.string().required('Required'),
+});
 
 const App = () => {
   const classes = useStyles();
@@ -35,7 +61,44 @@ const App = () => {
           >
             <Form>
               <Grid container spacing={2}>
-                <Grid item xs={12}></Grid>
+                <Grid item xs={12}>
+                  <Typography>Your details</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Textfield name='firstName' label='First Name' />
+                </Grid>
+                <Grid item xs={6}>
+                  <Textfield name='lastName' label='Last Name' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Textfield name='email' label='Email' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Textfield name='phone' label='Phone Number' />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography>Adress</Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Textfield name='addressLine1' label='addressLine1' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Textfield name='addressLine2' label='addressLine2' />
+                </Grid>
+                <Grid item xs={6}>
+                  <Textfield name='city' label='city' />
+                </Grid>
+                <Grid item xs={6}>
+                  <Textfield name='state' label='state' />
+                </Grid>
+                <Grid item xs={12}>
+                  <Select name='country' label='country' options={countries} />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <Typography>Booking information</Typography>
+                </Grid>
               </Grid>
             </Form>
           </Formik>
